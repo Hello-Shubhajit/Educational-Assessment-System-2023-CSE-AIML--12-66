dcb87ba17d6dd88d98c71026d467fd3a28739cd1
0Standing0Man0 <ambarroy11@gmail.com>
2023-10-17 22:44:47 +0530

    Pushing all files

---
+++ home.php
@@ -0,0 +1,12 @@
+
+          <h1 class="page-header">Welcome to Admin Dashboard.</h1>
+          <img class="mb-4" src="favicon.png" alt="" width="280" height="70">
+          <style>
+      body{
+  background-image: url('bgm.jpeg');
+  background-size: cover;
+  background-position: center;
+
+      }</style>
+			
+			
